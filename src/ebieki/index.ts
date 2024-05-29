a8ad69c95d08c9c7d3cc8a4625d95070fa2e3c44
Ahmed Fasih <wuzzyview@gmail.com>
2024-05-28 20:29:34 -0700

    need kanji breakdowns that Wanikani doesn't have

--- src/ebieki/index.ts
+++ src/ebieki/index.ts
@@ -15,3 +15,16 @@ export const vocab: WithDistance[] = JSON.parse(
 export const dependencies: DependencyGraph = JSON.parse(
   readFileSync(join(__dirname, "wanikani-kanji-graph.json"), "utf8")
 );
+
+const EXTRA_KANJI_BREAKDOWNS: Record<string, string[]> = { 歪: ["不", "正"] };
+
+for (const kanji in EXTRA_KANJI_BREAKDOWNS) {
+  if (kanji in dependencies) continue;
+  const radicals = EXTRA_KANJI_BREAKDOWNS[kanji];
+  dependencies.kanjiToRadicals[kanji] = radicals;
+  for (const radical of radicals) {
+    if (!(radical in dependencies.radicalToKanjis))
+      dependencies.radicalToKanjis[radical] = [];
+    dependencies.radicalToKanjis[radical].push(kanji);
+  }
+}
